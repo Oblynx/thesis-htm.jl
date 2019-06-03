b39015c410311dfb40138fff3306587a9e8995cc
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2019-06-03 19:57:07 +0300

    Split the workflow latex into preamble/body

--- design_walkthrough/build_walkthrough.jl
+++ design_walkthrough/build_walkthrough.jl
@@ -2,8 +2,21 @@ using Weave
 using Lazy
 
 dir= "design_walkthrough"
+out_dir= joinpath(dir,"build/")
 jmd_files= @>> readdir(dir) map(f->joinpath(dir,f)) map(abspath) filter(s->match(r".*jmd",s)|>!isnothing)
 
-weave.(jmd_files,
-       out_path= joinpath(dir,"build/"),
-       doctype = "md2tex")
+#weave.(jmd_files, out_path= out_dir, doctype= "md2tex")
+
+out_files= @>> readdir(out_dir) map(f->joinpath(out_dir,f)) map(abspath) filter(s->match(r".*tex",s)|>!isnothing)
+
+
+for file in out_files
+  cd(()->`csplit --suppress-matched --elide-empty-files "$file" '/documentclass/' '/begin.document./' '/end.document./'`|> run,
+     out_dir)
+  filename= file|> basename|> splitext; filename= filename[1];
+  filename_preamble= filename*"_preamble.tex"
+  filename_body= filename*"_body.tex"
+  `mv "$out_dir/xx00" "$out_dir/$filename_preamble"`|> run
+  `mv "$out_dir/xx01" "$out_dir/$filename_body"`|> run
+end
+
