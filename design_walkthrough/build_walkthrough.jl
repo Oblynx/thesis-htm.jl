dc0b1516a8e8910b8d6ae333d510fadbf6099fec
Konstantinos Samaras-Tsakiris <ksamtsak@gmail.com>
2019-06-04 03:50:40 +0300

    Weave greek

--- design_walkthrough/build_walkthrough.jl
+++ design_walkthrough/build_walkthrough.jl
@@ -1,22 +1,35 @@
 using Weave
 using Lazy
 
+# This has to happen at the local package
+#function uc2tex(s::String, escape=false)
+#    for key in keys(latex_symbols)
+#        if escape
+#            s = replace(s, latex_symbols[key] => "(*@\\ensuremath{$(texify(key))}@*)")
+#        else
+#            #s = replace(s, latex_symbols[key] => "\\ensuremath{$(texify(key))}")
+#        end
+#    end
+#    return s
+#end
+
 dir= "design_walkthrough"
 out_dir= joinpath(dir,"build/")
 jmd_files= @>> readdir(dir) map(f->joinpath(dir,f)) map(abspath) filter(s->match(r".*jmd",s)|>!isnothing)
 
-#weave.(jmd_files, out_path= out_dir, doctype= "md2tex")
+weave.(jmd_files, out_path= out_dir, doctype= "md2tex", latex_cmd="lualatex")
 
-out_files= @>> readdir(out_dir) map(f->joinpath(out_dir,f)) map(abspath) filter(s->match(r".*tex",s)|>!isnothing)
-
+out_files= @>> jmd_files map(basename) map(f->splitext(f)[1]) map(s->s*".tex") map(f->joinpath(out_dir,f)) map(abspath)
 
 for file in out_files
-  cd(()->`csplit --suppress-matched --elide-empty-files "$file" '/documentclass/' '/begin.document./' '/end.document./'`|> run,
+  println(file)
+  cd(()->`csplit --suppress-matched --elide-empty-files "$file" '/begin.document./' '/end.document./'`|> run,
      out_dir)
   filename= file|> basename|> splitext; filename= filename[1];
   filename_preamble= filename*"_preamble.tex"
   filename_body= filename*"_body.tex"
   `mv "$out_dir/xx00" "$out_dir/$filename_preamble"`|> run
   `mv "$out_dir/xx01" "$out_dir/$filename_body"`|> run
+  `sed -i -E '/usepackage.*(geometry|lmodern|fontenc)/d' "$out_dir/$filename_preamble"`|> run
+  `sed -i -E '/\\documentclass/d' "$out_dir/$filename_preamble"`|> run
 end
-
